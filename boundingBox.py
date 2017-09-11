0122ab20e323157d0f9367e0d2886cec4739bd76
Suvash Vishowkarma <suvashvishowkarma@gmail.com>
2017-09-11 19:39:08 +0545

    Logic for iterating over each box for segmentation

--- boundingBox.py
+++ boundingBox.py
@@ -5,28 +5,55 @@ img = cv2.imread('E:/_nepali char/test1.jpg')
 img_gray= cv2.imread('E:/_nepali char/test1.jpg',cv2.IMREAD_GRAYSCALE)
 
 ret, thresh = cv2.threshold(img_gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-imga ,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+imga ,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 print(len(contours))
 for i in range(0, len(contours)):
-    area = cv2.contourArea(contours[i]);
+    area = cv2.contourArea(contours[i])
     if i == 0:
         maxArea = area
-        index = i
+        bigIndex = i
     if area > maxArea:
         maxArea = area
-        index = i
+        bigIndex = i
 
-print(index)
+print(bigIndex)
 # cv2.drawContours(img, contours[index], -1, (255,0,255),2, cv2.LINE_AA, maxLevel=2)
-print(hierarchy[0])
+print(hierarchy)
 # maxAreaContour = hierarchy[index]
 # print(maxAreaContour)
-firstChildIndex = hierarchy[0][2][2]
+i = hierarchy[0][bigIndex][2]
+count = 0
+while True:
+	count+=1
+	boxArea = cv2.contourArea(contours[i])
+	if boxArea < img.size * 1/100 :
+		i = hierarchy[0][i][0]
+		continue
+	childIndex = hierarchy[0][i][2]
+	while True:
+		contentArea = cv2.contourArea(contours[childIndex])
+		if contentArea >= img.size * 0.25/100:
+			cv2.drawContours(img, contours[childIndex], -1, (255,0,255),2, cv2.LINE_AA, maxLevel=2)
+			cv2.imwrite('convehull.jpg',img)
+		if(hierarchy[0][childIndex][0] == -1):
+			break
+		childIndex = hierarchy[0][childIndex][0]
+	print(count)
+	i = hierarchy[0][i][0]
+	if(hierarchy[0][i][0]== -1):
+		break
+	# pass
+
+
+nextBoxIndex = hierarchy[0][0][2]
 # firtChild = contours[firstChildIndex]
+firstChildIndex = hierarchy[0][nextBoxIndex][2]
+
 print(firstChildIndex)
 
-cv2.drawContours(img, contours[firstChildIndex], -1, (255,0,255),2, cv2.LINE_AA, maxLevel=2)
+
+
 print(contours[2])
 
 cnt = contours[index]
