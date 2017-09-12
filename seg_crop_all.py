8dd2150e3f3d6578bdab0f8db75393e96e6dcd57
Suvash Vishowkarma <suvashvishowkarma@gmail.com>
2017-09-12 22:05:18 +0545

    Make path context aware in seg_crop_all

--- seg_crop_all.py
+++ seg_crop_all.py
@@ -1,6 +1,9 @@
 from boundingBox import seg_crop
 import os
 
-for f in os.listdir('D:/_nepali char/data'):
-    if os.path.isfile(os.path.join('D:/_nepali char/data',f)):
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+for f in os.listdir(os.path.join(BASE_DIR,'data')):
+    if os.path.isfile(os.path.join(BASE_DIR+'/data',f)):
         seg_crop(f)
